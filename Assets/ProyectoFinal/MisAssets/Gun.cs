b3eda6fa413800ecb288070ec93a81ccf4055fe8
javimu06 <javimu06@ucm.es>
2022-04-25 03:22:54 +0200

    WizzarKitchen con Player

---
+++ Assets/ProyectoFinal/MisAssets/Gun.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+public class Gun : MonoBehaviour
+{
+    public SteamVR_Action_Boolean fireAction;
+    public GameObject bullet;
+    public Transform barrelPivot;
+    public float shootingSpeed = 1;
+    //public GameObject muzzleFlash;
+
+    private Animator animator;
+    private Interactable interactable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //muzzleFlash.SetActive(false);
+        interactable = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactable.attachedToHand != null)
+        {
+            gameObject.GetComponent<Collider>().enabled = false;
+            SteamVR_Input_Sources source = interactable.attachedToHand.handType;
+
+            if (fireAction[source].stateDown)
+                Fire();
+        }
+        else
+            gameObject.GetComponent<Collider>().enabled = true;
+
+    }
+
+    void Fire()
+    {
+        Debug.Log("Fire");
+        Rigidbody bulletrb = Instantiate(bullet, barrelPivot.position, barrelPivot.rotation).GetComponent<Rigidbody>();
+        bulletrb.velocity = barrelPivot.forward * shootingSpeed;
+        //muzzleFlash.SetActive(true);
+    }
+}
