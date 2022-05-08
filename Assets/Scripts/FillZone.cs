1631752df1799514ab3e9dbd29c304ee13ae720a
oscar <osgarc05@ucm.es>
2022-05-08 20:11:01 +0200

    Toma grifo guapo que os he hecho

--- Assets/Scripts/FillZone.cs
+++ Assets/Scripts/FillZone.cs
@@ -7,9 +7,20 @@ public class FillZone : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log(other.name);
         //Si metes el cubo dentro de la zona...
         if (other.GetComponent<GlassFill>() != null)
+        {
             //Se rellena
             other.GetComponent<GlassFill>().fillBottle();
+        }
+    }
+    private void OnEnable()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+    private void OnDisable()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 }
