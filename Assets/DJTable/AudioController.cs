11ae0b675a3b1f383388245a553ee393d047e5db
javimu06 <javimu06@ucm.es>
2022-05-09 04:19:57 +0200

    el poder

--- Assets/DJTable/AudioController.cs
+++ Assets/DJTable/AudioController.cs
@@ -7,10 +7,32 @@ public class AudioController : MonoBehaviour
 {
     public AudioSource audiosource;
     public AudioMixerGroup audiomixergroup;
-    
+
+    [SerializeField]
+    bool shuffle;
+    public void changeShuffle() { audiosource.Stop(); shuffle = !shuffle; }
+
+    public List<AudioClip> audioClips;
+
+
     // Start is called before the first frame update
     void Start()
     {
         audiosource.outputAudioMixerGroup = audiomixergroup;
+        shuffle = true;
+    }
+
+    private void Update()
+    {
+        if (shuffle)
+        {
+            if (!audiosource.isPlaying)
+            {
+                int r = Random.Range(0, audioClips.Count);
+                AudioClip clip = audioClips[r];
+                audiosource.clip = clip;
+                audiosource.PlayOneShot(clip);
+            }
+        }
     }
 }
